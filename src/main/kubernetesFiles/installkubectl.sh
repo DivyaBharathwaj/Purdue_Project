8c9e8da4d1c809bb072aba24f1a8929cdf04edd7
DivyaBharathwaj <142706376+DivyaBharathwaj@users.noreply.github.com>
2024-05-01 17:01:22 +0530

    Create installkubectl.sh

---
+++ src/main/kubernetesFiles/installkubectl.sh
@@ -0,0 +1,10 @@
+ReadyMade kubernetes cluster:
+===============================================
+
+On GCP search for kubernetes clusters
+
+Click create → click on switch to standard cluster
+A cluster with 3 worker nodes will be created.
+Click on Connect to connect to the cluster
+
+Connect to cluster via command line -> copy the command and click RUn in cloud shell
